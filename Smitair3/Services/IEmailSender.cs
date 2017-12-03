accb3fe117c88a778deba9de956e41d679ac0af0
MarcinHorczak <MarcinHorczak@users.noreply.github.com>
2017-12-03 23:05:11 +0100

    Add project files.

---
+++ Smitair3/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Smitair3.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
